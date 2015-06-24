63d8b6b6616c08349fca260511c3b985a3ea41f3
vvisx <vvisx@allstate.com>
2015-06-24 12:47:52 -0500

    Initial commit

---
+++ TestLK/TestLK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestLK
+//
+//  Created by Vijay Viswas on 6/16/15.
+//  Copyright (c) 2015 Vijay Viswas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestLK/TestLK/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestLK
+//
+//  Created by Vijay Viswas on 6/16/15.
+//  Copyright (c) 2015 Vijay Viswas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
